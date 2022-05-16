d1dce60d935ef662b0804328947ff6e5a343908f
Ale <alegonzalez1993@gmail.com>
2022-05-15 22:59:41 -0400

    Project Finished

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,64 @@
 <template>
-<div>
-  <h1>Random Gif Cat</h1>
-  <form @submit.prevent="obtenerGatito">
-    <div class="input">
-      <label for="title">Titulo: </label>
-      <input type="text" name="title" v-model="title">
+  <div class="container mx-auto mt-10 flex flex-col items-center">
+    <h1 class="text-4xl mb-10 text-sky-700">Random Gif Cat</h1>
+    <div class="grid grid-cols-4 sm:grid-cols-2 gap-6">
+      <div>
+        <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm w-4/5" type="text" v-model="title" placeholder="Titulo" />
+      </div>
+      <div>
+        <select v-model="filtro" class="italic text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm w-4/5">
+          <option value="">Sin filtro</option>
+          <option
+            v-for="(filtro, index) in filtros"
+            :key="index"
+            :value="filtro"
+            v-text="filtro"
+          ></option>
+        </select>
+      </div>
+      <div>
+        <input type="number" v-model="size" placeholder="Tamaño texto" class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm w-4/5" />
+      </div>
+      <div class="flex flex-row items-center justify-between">
+        <select name="color" v-model="color" class="italic text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm w-4/5">
+          <option value="white">Color</option>
+          <option
+            v-for="(color, index) in colors"
+            :key="index"
+            :value="color"
+            v-text="color"
+          ></option>
+        </select>
+        <div class="circulo border-2 rounded-full bg-red-400 w-6 h-6" :style="{ 'background-color': `${this.color}` }"></div>
+      </div>
     </div>
-    <div class="input">
-      <label for="filter">Filtro: </label>
-      <select v-model="filtro"> 
-        <option v-for="(filtro, index) in filtros" :key="index" :value="filtro" v-text="filtro">
-        </option> 
-      </select>
-    </div>
-    <div class="input">
-      <label for="color">Color: </label>
-      <select name="color" v-model="color">
-        <option v-for="(color, index) in colors" :key="index" :value="color" v-text="color">
-        </option>
-      </select>
-    </div>
-    <div class="input">
-      <label for="size">Tamaño: </label>
-      <input type="number" name="size" v-model="size">
-    </div>
-    <button type="submit">Obtener mi gatito</button>
-  </form>
-  <img :src="url" alt="">
-</div>
+    <button @click="getGif" class="my-10 inline-block px-6 py-2.5 bg-sky-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Obtener mi gatito</button>
+    <img :src="gif" alt="" width="400" height="400" />
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
-    url: '',
-    title: '',
-    filtro: 'blur',
-    filtros: ['blur','mono','sepia','negative','paint','pixel'],
-    color: '',
-    colors: ['white', 'red', 'green'],
-    size: ''
+    title: "",
+    filtro: "",
+    filtros: ["blur", "mono", "sepia", "negative", "paint"],
+    color: "white",
+    colors: ["white", "red", "green", "blue", "yellow"],
+    size: "",
+    gif: "",
   }),
   methods: {
-    obtenerGatito() {
-      this.url = `https://cataas.com/cat/says/${this.title}?size=${this.size}&color=${this.color}?filter=${this.filtro}`
-      console.log(this.url)
-      // fetch(`https://cataas.com/cat/says/${this.title}?size=${this.size}&color=${this.color}?filter=${this.filtro}`)
-      //   .then(response => response.json())
-      //   .then(data => console.log(data))
-    }
-  }
-}
+    getGif(){
+      this.gif = `https://cataas.com/cat/gif/says/${this.title}?filter=${this.filtro}&color=${this.color}&size=${this.size}&type=or`
+    },
+  },
+};
 </script>
 
 <style>
-.input{
-  display: flex;
-  gap: 10px;
-  margin-bottom: 10px;
+* {
+  font-family: 'Raleway', sans-serif;
 }
-
 </style>
